dde949e772b7ce6db0eafc0ed26376c00ee57bf3
Justin W. Flory <me@justinwflory.com>
2015-01-07 07:00:43 -0500

    Initialize main class and plugin files

--- src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
+++ src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
@@ -1,1 +1,19 @@
-// TODO Add things!
+/*
+ * Copyright
+ */
+package com.crystalcraftmc.pvpstorm;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PvPStorm extends JavaPlugin {
+    public void onEnable() {
+        getLogger().info(ChatColor.AQUA + "PvP Storm has been initialized!");
+    }
+
+    public void onDisable() {
+        getLogger().info(ChatColor.RED + "PvP Storm has been stopped by the server.");
+    }
+
+    // TODO Write methods as described from Trello. Update on Slack as necessary!
+}
