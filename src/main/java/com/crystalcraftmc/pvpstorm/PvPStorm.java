54d333f278b97dbafd2fb0d0cde3e4e0f872a7f8
Justin W. Flory <me@justinwflory.com>
2015-01-07 20:58:03 -0500

    Create outline of all commands and set up listeners

--- src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
+++ src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
@@ -1,19 +1,87 @@
 /*
- * Copyright
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 CrystalCraftMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package com.crystalcraftmc.pvpstorm;
 
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PvPStorm extends JavaPlugin {
+    // TODO Implement mob boss health bar - perhaps in listener? NEEDS RESEARCH.
+    
     public void onEnable() {
         getLogger().info(ChatColor.AQUA + "PvP Storm has been initialized!");
+        getServer().getPluginManager().registerEvents(gameStart, this);
+        getServer().getPluginManager().registerEvents(gameEnd, this);
     }
 
     public void onDisable() {
         getLogger().info(ChatColor.RED + "PvP Storm has been stopped by the server.");
     }
 
-    // TODO Write methods as described from Trello. Update on Slack as necessary!
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("storm")) {
+            if (args.length == 1) {
+                return false;
+            } else if (args.length > 1) {
+                if (args[0].equalsIgnoreCase("start")) {
+                    Bukkit.broadcastMessage(ChatColor.DARK_RED + getConfig().getString("start-message"));
+                    // TODO Change weather to thunder + lightning?
+                    // TODO Implement a timer countdown?
+                    // TODO Alert the listener to begin counting who hits the Stormer, in order to give prizes at end
+                    return true;
+                } else if (args[0].equalsIgnoreCase("stop")) {
+                    Bukkit.broadcastMessage(ChatColor.AQUA + getConfig().getString("end-message"));
+                    // TODO Reset weather to sun?
+                    // TODO Alert listener to stop counting and give out awards
+                    return true;
+                } else if (args[0].equalsIgnoreCase("power")) {
+                    // TODO Block console from running these commands!
+                    if (args.length < 2) {
+                        // TODO Output a list of all possible powers to the user
+                        return true;
+                    } else if (args[1].equalsIgnoreCase("flare")) {
+                        // TODO Damage players in radius, temporarily lower health
+                        return true;
+                    } else if (args[1].equalsIgnoreCase("vanish")) {
+                        // TODO Make user invisible at expense of health
+                        return true;
+                    } else if (args[1].equalsIgnoreCase("timewarp")) {
+                        // TODO Instantly move the user backwards about 10 blocks, but add slowness for a few seconds
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    
+    private GameStartListener gameStart = new GameStartListener(this);
+    private GameEndListener gameEnd = new GameEndListener(this);
 }
