f5d99e7940b32a638dbad666f09720afbb7fbc31
Ivan <Jacc734@users.noreply.github.com>
2015-01-09 00:53:57 -0500

    Sets, No Commands, Get/Set/Change Stormer

--- src/main/java/com/crystalcraftmc/pvpstorm/GameStartListener.java
+++ src/main/java/com/crystalcraftmc/pvpstorm/GameStartListener.java
@@ -25,6 +25,8 @@
 
 package com.crystalcraftmc.pvpstorm;
 
+import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
@@ -34,42 +36,59 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 
-public class GameStartListener implements Listener, CommandExecutor {
-    PvPStorm plugin;
-    private ArrayList<Player> playersWhoHitStormer; //holds array of Player objects recording who hit flory
-    private boolean enablePlayerHitStormerCounting; //enables onEntityVsEntity listener method
-  
-    public GameStartListener(PvPStorm plugin) {
-        this.plugin = plugin;
-        playersWhoHitStormer = new ArrayList<Player>();
-        enablePlayerHitStormerCounting = false;
+public class GameStartListener implements Listener  {
+	//PvPStorm plugin;
+	private String stormerName;//Added this so that the stormer could be edited in config file for others. - jacc
+	private Set<String> playersWhoHitStormer; //changed to set and string - Players objects are memory hogs -jacc
+	private boolean enablePlayerHitStormerCounting; //enables onEntityVsEntity listener method
+	
+	//IGNORE this method - Only for use when an instances in called in the main plugin class. And only when Stormer name is needed to add to check permissions.
+	public String getStormer() {
+        return stormerName;
     }
+    //IGNORE This method is NOT for human use. (It will be called once in the onEnable() method of the main plugin class file.)
+	public void setStormer(String name) {
+       this.stormerName = name;
+    }
+	
+	public GameStartListener(PvPStorm plugin) {
+		this.plugin = plugin;
+		playersWhoHitStormer = new TreeSet<String>();
+		enablePlayerHitStormerCounting = false;
+	}
     
     
     // WARNING: The following listener code was me experimenting. Needs research! - jf
     // WARNING#2:  I added a few commands to comply with my untested algorithm - jwood
     // Not on my computer at the moment - though I'll be able to test code tomorrow - jwood
-    @EventHandler
-    public void onStart(PlayerCommandPreprocessEvent useCommand) {
-        if (useCommand.equals("start")) {
-            // TODO Implement counts of which players hit the Stormer - might use another event type?
-            enablePlayerHitStormerCounting = true;
-        }
-        else if(useCommand.equals("stop")) {
-            enablePlayerHitStormerCounting = false;
-        }
-        else if(useCommand.equals("reset")) {
-            enablePlayerHitStormerCounting = false;
-            playersWhoHitStormer = new ArrayList<Player>(); //empties the arraylist
-        }
-        else if(useCommand.equals("getArrayList")) {
-            //this command is mostly for testing / debugging
-            for(int i = 0; i < playersWhoHitStormer.size(); i++) {
-                useCommand.getPlayer().sendMessage(playersWhoHitStormer.get(i).getName());
-            }
-        }
-    }
+    //WARNING#3: I made a few changes... so I am sorry for the confusion or intrusion into current edits-jacc
+	
+	//TODO MOVE THESE... commands to plugin side? and rework so that they connect idk just an idea? commented it out for now - jacc
+	
+	/*@EventHandler
+	public void onStart(PlayerCommandPreprocessEvent useCommand) {
+		if (useCommand.equals("start")) {
+			// TODO Implement counts of which players hit the Stormer - might use another event type?
+			enablePlayerHitStormerCounting = true;
+		}
+		else if(useCommand.equals("stop")) {
+			enablePlayerHitStormerCounting = false;
+		}
+		else if(useCommand.equals("reset")) {
+			enablePlayerHitStormerCounting = false;
+			playersWhoHitStormer.clear();//clears the set
+		}
+		else if(useCommand.equals("getSet")) {//btw If this doesnt work I can try something else.
+			//this command is mostly for testing / debugging
+			if ((String[]) playersWhoHitStormer.toArray() == null) return;
+			useCommand.getPlayer().sendMessage((String[]) playersWhoHitStormer.toArray());
+		}
+		return;
+	}
+	*/
     
     // Warning below listener is untested! Though I will explain the idea - jwood
     //listener is called when one entity hits another
@@ -85,37 +104,19 @@ public class GameStartListener implements Listener, CommandExecutor {
     //upon the "stop" command. (added a "reset" command which turns the boolean flag variable
     //playerHitStormerCounting to false (disables listener below), and empties the current arraylist)
     //again, I note that this method is untested - jwood
-    @EventHandler
-    public void onEntityVsEntity(EntityDamageByEntityEvent event) {
-        if(enablePlayerHitStormerCounting) {
-            if(event.getDamager() instanceof Player) {
-                Bukkit.broadcastMessage("debug: damager is an instance of player");
-                if(event.getEntity() instanceof Player) {
-                    Bukkit.broadcastMessage("debug: damagee is an instance of player");
-                    Player damagee = (Player)event.getEntity();
-                    Player damager = (Player)event.getDamager();
-                    if(damagee.getName().equals("jflory7") || damagee.getName().equals("Jwood9198")) {
-                        if(playersWhoHitStormer.size() == 0) {
-                            playersWhoHitStormer.add(damager);
-                            Bukkit.broadcastMessage("debug: player added to arraylist");
-                        }
-                        else {
-                            boolean isDamagersFirstHitOnStormer = true;
-                            for(int i = 0; i < playersWhoHitStormer.size(); i++) {
-                                if(damager.getName().equals(playersWhoHitStormer.get(i).getName())) {
-                                    isDamagersFirstHitOnStormer = false;
-                                }
-                            }
-                            if(isDamagersFirstHitOnStormer) {
-                                playersWhoHitStormer.add(damager);
-                                Bukkit.broadcastMessage("debug: player added to arraylist");
-                            }
-                        } 
-                    }  
-            
-                }  
-            }
-        }
-    
-    }
+    //TODO RE: Warning: Using sets resolves the issue as sets will not add an item that it already contains -jacc
+	@EventHandler
+	public void onEntityVsEntity(EntityDamageByEntityEvent event) {
+		if(enablePlayerHitStormerCounting) {
+			if(event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+				Bukkit.broadcastMessage(ChatColor.GOLD + "debug: Both damager AND damagee are an instances of a Player");
+				Player damagee = (Player)event.getEntity();
+				Player damager = (Player)event.getDamager();
+				if(damagee.getName().equals(stormerName) && !damagee.getName().equals(null)) {
+					playersWhoHitStormer.add(damager.getName());
+					Bukkit.broadcastMessage(ChatColor.GOLD + "debug: player added to set");
+				}
+			}
+		}
+	}
 }
