52911549b3c604816a613634f38aa995a76c7fcc
Justin W. Flory <me@justinwflory.com>
2015-02-10 10:59:47 -0500

    Create flare effect - damage nearby players and self

--- src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
+++ src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
@@ -1,28 +1,40 @@
 /*
- * Copyright 2015 CrystalCraftMC
+ * The MIT License (MIT)
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Copyright (c) 2015 CrystalCraftMC
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.crystalcraftmc.pvpstorm;
 
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
 
 public class PvPStorm extends JavaPlugin {
     // TODO Implement mob boss health bar - perhaps in listener? NEEDS RESEARCH.
@@ -40,6 +52,7 @@ public class PvPStorm extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player) sender;
         World world = p.getWorld();
+        Location loc = p.getLocation();
 
         if (cmd.getName().equalsIgnoreCase("storm")) {
             if (!(sender instanceof Player)) {
@@ -75,9 +88,12 @@ public class PvPStorm extends JavaPlugin {
                         return true;
                     } else if (args[1].equalsIgnoreCase("flare")) {
                         Bukkit.broadcastMessage("[INSERT PLAYER NAME VARIABLE] " + ChatColor.YELLOW + " used " + ChatColor.GOLD + " FLARE " + ChatColor.YELLOW + " ability!");
-                        // TODO Damage players in radius, temporarily lower health
-                        p.getNearbyEntities()
-                        [anyone who is nearby, not finished with this].setHealth(p.getHealth() - 2.0);
+                        loc = p.getLocation();
+                        Collection<? extends Player> players = Bukkit.getOnlinePlayers();
+                        for (Player nearbyPlayer : players) {
+                            if (nearbyPlayer.getLocation().distanceSquared(loc) <= 25) nearbyPlayer.setHealth(nearbyPlayer.getHealth() - 2.0);
+                        }
+                        p.setHealth(p.getHealth() - 3.0);
                         return true;
                     } else if (args[1].equalsIgnoreCase("vanish")) {
                         Bukkit.broadcastMessage("[INSERT PLAYER NAME VARIABLE] " + ChatColor.YELLOW + " used " + ChatColor.GRAY + " VANISH " + ChatColor.YELLOW + " ability!");
