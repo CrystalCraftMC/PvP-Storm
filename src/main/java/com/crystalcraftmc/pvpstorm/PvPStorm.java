6eee82668c8bd1817afcc338a33a50678706efcb
Justin W. Flory <me@justinwflory.com>
2015-03-25 12:17:38 -0400

    Increase potion effect times for vanish and timewarp

--- src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
+++ src/main/java/com/crystalcraftmc/pvpstorm/PvPStorm.java
@@ -1,17 +1,26 @@
 /*
- * Copyright 2015 CrystalCraftMC
+ * The MIT License (MIT)
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Copyright (c) 2015 CrystalCraftMC
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.crystalcraftmc.pvpstorm;
@@ -102,17 +111,17 @@ public class PvPStorm extends JavaPlugin {
                         }
                         if (p.getHealth() > 3.0) p.setHealth(p.getHealth() - 3.0);
                         else if (p.getHealth() <= 1.0) {
-                            p.setHealth(0.0);
+                            p.setHealth(0.0D);
                             Bukkit.broadcastMessage(p.getDisplayName() + ChatColor.RED + "commit suicide with their own powers!");
                         }
                         else p.setHealth(1.0);
                         return true;
                     } else if (args[1].equalsIgnoreCase("vanish")) {
                         Bukkit.broadcastMessage(p.getDisplayName() + ChatColor.YELLOW + " used " + ChatColor.GRAY + " VANISH " + ChatColor.YELLOW + " ability!");
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 120, 0, false, false));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 3600, 0, false, false));
                         if (p.getHealth() > 5.0) p.setHealth(p.getHealth() - 5.0);
                         else if (p.getHealth() <= 1.0) {
-                            p.setHealth(0.0);
+                            p.setHealth(0.0D);
                             Bukkit.broadcastMessage(p.getDisplayName() + ChatColor.RED + "commit suicide with their own powers!");
                         } else p.setHealth(1.0);
                         return true;
@@ -121,7 +130,7 @@ public class PvPStorm extends JavaPlugin {
 
                         Bukkit.broadcastMessage(p.getDisplayName() + ChatColor.YELLOW + " used " + ChatColor.RED + " TIMEWARP " + ChatColor.YELLOW + " ability!");
                         p.setVelocity(new Vector(direction.getX() * -2.5, 1.2, direction.getZ() * -2.5));
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 180, 0));
+                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 300, 0));
                         return true;
                     }
                 } else {
